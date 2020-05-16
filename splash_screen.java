000771da9f2e18148d494b337916237f8f9ccc6c
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-16 20:42:38 +0530

    splash screen

---
+++ splash_screen.java
@@ -0,0 +1,18 @@
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+
+            }
+        },4000);
+    }
+}
