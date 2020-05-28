9d141b5b682d6d3e9e5c0f9e4bd8b24dbf366d8f
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-05-28 20:28:31 +0530

    5FeedbackActivity3.java
    contain the constructer ,getter ,setter method (feedback)

---
+++ 5FeedbackActivity3.java
@@ -0,0 +1,30 @@
+package com.example.topnews;
+
+public class feedbackActivity3 {
+
+    String name,comment;
+
+    public feedbackActivity3() {
+    }
+
+    public feedbackActivity3(String name, String comment) {
+        this.name = name;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
